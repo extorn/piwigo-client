72e8b0ee3d786018fe590f6fdd6b1bcc49a03bfa
Gareth <gareth_deli@yahoo.co.uk>
2020-02-05 17:38:36 +0000

    Allow user to force a language to use

--- app/src/free/java/delit/piwigoclient/ui/MyApplication.java
+++ app/src/free/java/delit/piwigoclient/ui/MyApplication.java
@@ -1,6 +1,15 @@
 package delit.piwigoclient.ui;
 
+import android.content.Context;
+
+import delit.piwigoclient.business.AppPreferences;
+
 public class MyApplication extends AbstractMyApplication {
+    @Override
+    protected String getDesiredLanguage(Context context) {
+        return AppPreferences.getDesiredLanguage(getPrefs(context), context);
+    }
+
     @Override
     protected void onAppCreate() {
     }
