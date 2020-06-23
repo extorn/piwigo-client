4265cbe33523245aabf5016664fef3e323d4855f
Gareth <gareth@localhost>
2020-06-23 15:25:44 +0100

    added privacy page for the paid app

--- app/src/paid/java/delit/piwigoclient/ui/MainActivity.java
+++ app/src/paid/java/delit/piwigoclient/ui/MainActivity.java
@@ -1,23 +1,30 @@
 package delit.piwigoclient.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.appcompat.view.ContextThemeWrapper;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.HashSet;
 
+import delit.libs.ui.view.ProgressIndicator;
 import delit.libs.ui.view.recycler.BaseRecyclerViewAdapterPreferences;
 import delit.libs.util.VersionUtils;
 import delit.piwigoclient.R;
 import delit.piwigoclient.business.ConnectionPreferences;
 import delit.piwigoclient.model.piwigo.PiwigoSessionDetails;
 import delit.piwigoclient.model.piwigo.Tag;
-import delit.piwigoclient.ui.events.BackgroundUploadStartedEvent;
-import delit.piwigoclient.ui.events.BackgroundUploadStoppedEvent;
-import delit.piwigoclient.ui.events.BackgroundUploadThreadCheckingForTasksEvent;
-import delit.piwigoclient.ui.events.BackgroundUploadThreadStartedEvent;
-import delit.piwigoclient.ui.events.BackgroundUploadThreadTerminatedEvent;
 import delit.piwigoclient.ui.events.ViewJobStatusDetailsEvent;
 import delit.piwigoclient.ui.events.ViewTagEvent;
 import delit.piwigoclient.ui.events.trackable.AutoUploadJobViewRequestedEvent;
@@ -86,7 +93,33 @@ public class MainActivity extends AbstractMainActivity {
     }
 
     protected void showPrivacy() {
-        getUIHelper().showOfQueueDialogMessage();
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(new ContextThemeWrapper(this, R.style.Theme_App_EditPages));
+        View view = LayoutInflater.from(dialogBuilder.getContext()).inflate(R.layout.layout_dialog_privacy_information, null);
+        ProgressIndicator progressIndicator = view.findViewById(R.id.website_loading_progress);
+        progressIndicator.showProgressIndicator(R.string.loading_please_wait, 0);
+        TextView privacyField = view.findViewById(R.id.privacy_policy);
+        privacyField.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse(v.getContext().getString(R.string.privacy_policy_uri));
+            intent.setDataAndType(uri, "text/html");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            v.getContext().startActivity(Intent.createChooser(intent, v.getContext().getString(R.string.open_link)));
+        });
+        WebView myWebView = view.findViewById(R.id.privacy_text);
+        myWebView.setWebChromeClient(new WebChromeClient(){
+
+            public void onProgressChanged(WebView view, int progress) {
+                progressIndicator.showProgressIndicator(R.string.loading_please_wait, progress * 100);
+                if(progress == 100) {
+                    progressIndicator.hideProgressIndicator();
+                }
+            }
+        });
+        myWebView.loadUrl(getString(R.string.privacy_policy_uri));
+        dialogBuilder.setView(view);
+        dialogBuilder.setPositiveButton(android.R.string.ok, (dialog, which) -> {});
+
+        dialogBuilder.show();
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
