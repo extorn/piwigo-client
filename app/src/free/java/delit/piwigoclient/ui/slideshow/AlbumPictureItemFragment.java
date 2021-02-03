9bef9a731c200cd5d2971b239f35dbff933d0d86
Gareth <g01z@yahoo.co.uk>
2021-02-03 09:52:28 +0000

    generics

--- app/src/free/java/delit/piwigoclient/ui/slideshow/AlbumPictureItemFragment.java
+++ app/src/free/java/delit/piwigoclient/ui/slideshow/AlbumPictureItemFragment.java
@@ -5,8 +5,8 @@ import delit.piwigoclient.ui.common.FragmentUIHelper;
 import delit.piwigoclient.ui.model.ViewModelContainer;
 
 public class AlbumPictureItemFragment<F extends AlbumPictureItemFragment<F,FUIH,T>, FUIH extends FragmentUIHelper<FUIH, F>, T extends PictureResourceItem> extends AbstractAlbumPictureItemFragment<F,FUIH,T> {
-    public static AlbumPictureItemFragment<?,?> newInstance(Class<? extends ViewModelContainer> modelType, long albumId, long albumItemId, int albumResourceItemIdx, int albumResourceItemCount, long totalResourceItemCount) {
-        AlbumPictureItemFragment<?,?> fragment = new AlbumPictureItemFragment<>();
+    public static AlbumPictureItemFragment<?,?,?> newInstance(Class<? extends ViewModelContainer> modelType, long albumId, long albumItemId, int albumResourceItemIdx, int albumResourceItemCount, long totalResourceItemCount) {
+        AlbumPictureItemFragment<?,?,?> fragment = new AlbumPictureItemFragment<>();
         fragment.setArguments(buildArgs(modelType, albumId, albumItemId, albumResourceItemIdx, albumResourceItemCount, totalResourceItemCount));
         return fragment;
     }
