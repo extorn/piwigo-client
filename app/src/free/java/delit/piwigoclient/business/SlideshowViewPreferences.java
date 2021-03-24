f09f9e9051df46293b4ddd631d948f7a8ad61702
Gareth <g01z@yahoo.co.uk>
2021-03-24 09:01:24 +0000

    needed for free version to compile

--- app/src/free/java/delit/piwigoclient/business/SlideshowViewPreferences.java
+++ app/src/free/java/delit/piwigoclient/business/SlideshowViewPreferences.java
@@ -6,4 +6,7 @@ import android.content.SharedPreferences;
 import delit.piwigoclient.R;
 
 public class SlideshowViewPreferences extends BaseSlideshowViewPreferences {
+    public static boolean isShowItemDetailOnSlideshowItemOpen(SharedPreferences prefs, Context context) {
+        return context.getResources().getBoolean(R.bool.preference_gallery_slideshow_item_detail_show_on_item_show_default);
+    }
 }
